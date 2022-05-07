840e98df04d70fbfe92467449b85c5aed173f0fd
Kles <klesta.luli@gmail.com>
2022-05-07 16:36:43 +0300

    added react portal for modal overlay and backdrop

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,23 +1,43 @@
+import ReactDOM from 'react-dom'
 import classes from "./ErrorModal.module.css"
 import Button from "./Button"
 import Card from "./Card"
-import { Fragment } from "react"
+import React, { Fragment } from "react"
+
+function Backdrop(props) {
+    return <div className={classes.backdrop} onClick={props.onDismiss} />
+}
+
+function ModalOverlay(props) {
+    return (
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.actions}>
+                <Button onClick={props.onDismiss}>Okay</Button>
+            </footer>
+        </Card>
+    )
+}
 
 export default function ErrorModal(props) {
     return (
         <Fragment>
-            <div className={classes.backdrop} onClick={props.onDismiss} />
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onDismiss}>Okay</Button>
-                </footer>
-            </Card>
+            {ReactDOM.createPortal(
+                <Backdrop onDismiss={props.onDismiss} />,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay 
+                    title={props.title}
+                    message={props.message}
+                    onDismiss={props.onDismiss} />,
+                    document.getElementById('overlay-root')
+            )}
         </Fragment>
     )
 }
